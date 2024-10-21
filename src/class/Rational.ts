39ce5f37753375e4218fbba0f6b5d612a341d4b2
willybigboat <411630337@o365.tku.edu.tw>
2024-10-22 02:56:29 +0800

    有喔有在難的10/22 2:56

--- src/class/Rational.ts
+++ src/class/Rational.ts
@@ -3,4 +3,65 @@
  */
 export class Rational {
     // todo: ...
+    numerator: number;
+    denominator: number;
+
+    constructor(numerator: number, denominator: number) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.normalize();
+    }
+
+    getNumerator(): number {
+        return this.numerator;
+    }
+
+    getDenominator(): number {
+        return this.denominator;
+    }
+
+    normalize(): Rational {
+        let gcd = 0;
+        for (let i = 1; i <= this.numerator && i <= this.denominator; i++) {
+            if (this.numerator % i == 0 && this.denominator % i == 0) {
+                gcd = i;
+            }
+        }
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+        if (this.denominator < 0) {
+            this.numerator = -this.numerator;
+            this.denominator = -this.denominator;
+        }
+        return this;
+    }
+
+    isWhole(): boolean {
+        return this.denominator == 1 || this.numerator % this.denominator == 0;
+    }
+
+    isDecimal(): boolean {
+        return !this.isWhole();
+    }
+
+
+    equals(r: Rational): boolean {
+        return this.numerator == r.getNumerator() && this.denominator == r.getDenominator();
+    }
+
+    static _parseRational(numChars: string[], denomChars: string[]): Rational {
+        const num = parseInt(numChars.join(''), 10);
+        const denom = parseInt(denomChars.join(''), 10);
+        return new Rational(num, denom);
+    }
+
+    static parseRational(str: string): Rational {
+        const parts = str.split("/");
+        const num = parseInt(parts[0]);
+        const denom = parseInt(parts[1]);
+        return new Rational(num, denom);
+    }
+    toString(): string {
+        return `${this.numerator}/${this.denominator}`;
+    }
 }
